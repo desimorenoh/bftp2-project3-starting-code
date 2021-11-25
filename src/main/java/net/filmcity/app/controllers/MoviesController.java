cea59adde4fa6c3d318608fc8a57d541703daffe
AnnaGirona89 <annartworks@gmail.com>
2021-11-25 12:21:23 +0100

    post1

--- src/main/java/net/filmcity/app/controllers/MoviesController.java
+++ src/main/java/net/filmcity/app/controllers/MoviesController.java
@@ -4,6 +4,8 @@ import net.filmcity.app.domain.Movie;
 import net.filmcity.app.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,4 +24,17 @@ public class MoviesController {
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+
+    @PostMapping("/movies")
+    public Movie addMovie (@RequestBody Movie movie) {
+        movieRepository.save(movie);
+        return movie;
+    }
+
+
+
+
+
+
+
 }
